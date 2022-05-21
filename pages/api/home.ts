abb3d5fca21879b37b8a929c10c79b795adaeabb
Caick da Silva Galvão <88976384+Caickolas@users.noreply.github.com>
2022-05-21 17:22:59 -0300

    Update home.ts

--- pages/api/home.ts
+++ pages/api/home.ts
@@ -3,36 +3,43 @@ import type { RespostaPadraoMsg } from '../../types/RespostaPadraoMsg';
 import { conectarMongoDB } from '../../middlewares/conectarMongoDB';
 import { UsuarioModel } from "../../models/UsuarioModel";
 import { ModuloModel } from '../../models/ModuloModel';
-import modulo from "./modulo";
 
 
 
 
 
-const feedEndpoint = async (req : NextApiRequest, res : NextApiResponse<RespostaPadraoMsg | any>) => {
+
+const homeEndpoint = async (req : NextApiRequest, res : NextApiResponse<RespostaPadraoMsg | any>) => {
     try{
         if(req.method === 'GET'){
             if(req?.query?.id){
                 const modulo = await ModuloModel
                 .findById(req?.query?.id)
-                .sort({data : -1});
+                .sort(1);
                 if(!modulo){
                     res.status(400).json({ erro: 'Modulo nao encontrado' })
                 }
 
             }else{
                 const {userId} = req.query;
-                const admin = await UsuarioModel.findById(userId)
-                const modulo = await ModuloModel.find({idUsuario : admin._id})
+                const usuarioLogado = await UsuarioModel.findById(userId);
+                if(!usuarioLogado){
+                    return res.status(400).json({erro : 'Usuario nao encontrado'});
+                }
+                const modulo = await ModuloModel.find({idUsuario : usuarioLogado._id})
                 const modulosIds = await modulo.map(m => m._id);
 
                 const modulos = await ModuloModel.find({
-                    
+                    idModulo : modulosIds
                 })
-
+                .sort(1)
             }
+            return res.status(405).json({erro: 'Metodo informado não é valido'})
         }
     }catch(e){
-        
+        console.log(e)
     }
-}
+    res.status(400).json({erro: 'Nao foi possivel obter o home'});
+};
+
+export default (conectarMongoDB(homeEndpoint));
