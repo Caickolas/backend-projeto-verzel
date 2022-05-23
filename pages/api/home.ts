e89e66877a462b40a7b0dd652e3919abc4375d9c
Caick da Silva Galvão <88976384+Caickolas@users.noreply.github.com>
2022-05-23 02:13:32 -0300

    terminando home e começando put e delete

--- pages/api/home.ts
+++ pages/api/home.ts
@@ -1,7 +1,7 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 import type { RespostaPadraoMsg } from '../../types/RespostaPadraoMsg';
 import { conectarMongoDB } from '../../middlewares/conectarMongoDB';
-import { UsuarioModel } from "../../models/UsuarioModel";
+import { AulaModel } from "../../models/AulaModel";
 import { ModuloModel } from '../../models/ModuloModel';
 
 
@@ -13,31 +13,23 @@ const homeEndpoint = async (req : NextApiRequest, res : NextApiResponse<Resposta
     try{
         if(req.method === 'GET'){
             if(req?.query?.id){
-                const modulo = await ModuloModel
-                .findById(req?.query?.id)
+                const modulo = await ModuloModel.findById(req?.query?.id)
                 
                 if(!modulo){
                     res.status(400).json({ erro: 'Modulo nao encontrado' })
                 }
+                
+                const aulas = await AulaModel.find({idModulo : req?.query?.id})
+                .sort({nome : 1})
 
-                const modulos = await ModuloModel.find({idModulo: modulo._id})
-                
-                
-                return res.status(200).json(modulos)
+                return res.status(200).json(aulas)
 
             }else{
-                const {userId} = req.query;
-                const usuario = await UsuarioModel.findById(userId)
-                if(!usuario){
-                    return res.status(400).json({erro : 'Usuario nao encontrado'});
-                }
-                const modulo = await ModuloModel.find({idUsuario : usuario._id})
-                const modulosIds = await modulo.map(m => m._id);
+                const modulos = await ModuloModel.find()
+                .sort({nome : 1})
 
-                const modulos = await ModuloModel.find({
-                    idModulos : modulosIds
-                })
-                return res.status(200).json(modulos)
+                return res.status(200).json({modulos})
+                
             }
         }
         return res.status(405).json({erro: 'Metodo informado não é valido'})
@@ -46,5 +38,6 @@ const homeEndpoint = async (req : NextApiRequest, res : NextApiResponse<Resposta
     }
     res.status(400).json({erro: 'Nao foi possivel obter o home'});
 };
+
 
 export default (conectarMongoDB(homeEndpoint));
--- pages/api/modulo.ts
+++ pages/api/modulo.ts
@@ -1,55 +1,68 @@
-import type {NextApiResponse} from "next";
+import type { NextApiRequest, NextApiResponse } from "next";
 import type {RespostaPadraoMsg} from '../../types/RespostaPadraoMsg';
 import {conectarMongoDB} from '../../middlewares/conectarMongoDB'
 import {validarTokenJWT} from '../../middlewares/validarTokenJWT'
 import {UsuarioModel} from '../../models/UsuarioModel'
 import {ModuloModel} from '../../models/ModuloModel'
+import nc from "next-connect";
 
 
-const endpointModulo = async (req: any, res: NextApiResponse<RespostaPadraoMsg>) => {
-    try{        
-            if(req.method === 'POST'){
+const handler = nc() 
+    .post(async (req: NextApiRequest, res: NextApiResponse<RespostaPadraoMsg>) => {
+        try{        
 
-            const {userId} = req.query;
-            const usuario = await UsuarioModel.findById(userId)
+                const {userId} = req.query;
+                const usuario = await UsuarioModel.findById(userId)
 
-            if(!usuario){
-                return res.status(400).json({ erro: 'usuario não encontrado' }) 
+                if(!usuario){
+                    return res.status(400).json({ erro: 'usuario não encontrado' }) 
+                }
+
+                if(usuario.role !== 'admin' ){
+                    return res.status(401).json({ erro: 'Você não tem permissão' })
+                }
+
+                const {nome} = req?.body;
+
+                if (!req || !req.body){
+                    return res.status(400).json({ erro: 'Parametro de entrada não informado' })
+                }
+
+                if (!nome || nome.length < 1) {
+                    return res.status(400).json({ erro: 'Nome não é valido' })
+                }
+                
+                const Modulo = { 
+                    idUsuario : usuario._id,
+                    nome 
+                };  
+
+                    await ModuloModel.create(Modulo);
+                    return res.status(200).json({ msg: 'Modulo cadastrado com sucesso!' })
+                             
+            }catch(e){
+                console.log(e);
+                return res.status(500).json({ msg: 'erro ao cadastrar modulo' });
             }
+        })
+    .put(async (req: NextApiRequest, res: NextApiResponse<RespostaPadraoMsg> ) => {
+        try {
+            if(req?.query?.id){
+                const modulo = await ModuloModel.findById(req?.query?.id)
 
-            if(usuario.role !== 'admin' ){
-                return res.status(401).json({ erro: 'Você não tem permissão' })
-            }
-
-            const {nome} = req?.body;
-
-            if (!req || !req.body){
-                return res.status(400).json({ erro: 'Parametro de entrada não informado' })
-            }
-
-            if (!nome || nome.length < 1) {
-                return res.status(400).json({ erro: 'Nome não é valido' })
-            }
-
-            
-            const Modulo = { 
-                idUsuario : usuario._id,
-                nome 
+                const{nome} = req.body;
+            if(nome && nome.length > 2){
+                usuario.nome = nome
             };
-        
-                
-                await ModuloModel.create(Modulo);
-                return res.status(200).json({ msg: 'Modulo cadastrado com sucesso!' })
-            
+            }else{
+                res.status(400).json({ erro: 'Modulo nao encontrado' })
             }
-                return res.status(405).json({ erro: 'Metodo informado nao e valid' }); 
-                
-        
-        }catch(e){
-            console.log(e);
-            return res.status(500).json({ msg: 'erro ao cadastrar modulo' });
+            
+        } catch (e) {
+            console.log(e)
+            return res.status(500).json({ msg: 'erro ao atualizar modulo' });           
         }
-    };
+    })
     
     
-export default (validarTokenJWT(conectarMongoDB(endpointModulo)));
+export default (validarTokenJWT(conectarMongoDB(handler)));
