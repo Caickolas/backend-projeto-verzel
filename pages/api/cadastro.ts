2f136a0032f06933f04215da1b11d1c75ae4ef32
Caick da Silva Galvão <88976384+Caickolas@users.noreply.github.com>
2022-05-24 17:13:16 -0300

    Update cadastro.ts

--- pages/api/cadastro.ts
+++ pages/api/cadastro.ts
@@ -9,35 +9,33 @@ import { politicaCORS } from '../../middlewares/politicaCORS';
 
 const endpointCadastro = async (req: NextApiRequest, res: NextApiResponse<RespostaPadraoMsg>) => {        
             if(req.method === 'POST'){
-            const usuario = req.body as CadastroRequisicao;
+            const {nome, email, senha} = req.body;
 
-            if (!usuario.nome ) {
+            if (!nome ) {
                 return res.status(400).json({ erro: 'informe um nome valido' });
             }
 
-            if (!usuario.email || usuario.email.length < 5
-                || !usuario.email.includes('@')
-                || !usuario.email.includes('.')) {
+            if (!email || email.length < 5
+                || !email.includes('@')
+                || !email.includes('.')) {
                 return res.status(400).json({ erro: 'Email invalido' });
             }
 
-            if (!usuario.senha || usuario.senha.length < 4) {
+            if (!senha || senha.length < 4) {
                 return res.status(400).json({ erro: 'Senha invalida' });
             }
 
             
             
-            const usuariosComMesmoEmail = await UsuarioModel.find({ email: usuario.email });
+            const usuariosComMesmoEmail = await UsuarioModel.find({ email: email });
             if (usuariosComMesmoEmail && usuariosComMesmoEmail.length > 0) {
                 return res.status(400).json({ erro: 'Ja existe uma conta com o email informado' });
             }
 
             const usuarioASerSalvo = {
-                nome: usuario.nome,
-                email: usuario.email,
-                senha: md5(usuario.senha),
-                role: usuario.role
-
+                nome: nome,
+                email: email,
+                senha: md5(senha),               
             }
             await UsuarioModel.create(usuarioASerSalvo);
             return res.status(200).json({ msg: 'Usuario criado com sucesso' });
